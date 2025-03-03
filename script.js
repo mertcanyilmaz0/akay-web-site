6fbd369646eaebcb0434cd63c5675069cfdf8f30
merd <oyunkafasimertcan@gmail.com>
2025-03-03 20:19:04 +0300

    Navbar Mobil Responsiveliği düzeltildi.

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-const menuBtn = document.querySelector("#menu-btn");
-const navbar = document.querySelector(".navbar");
-
-menuBtn.addEventListener("click", function() {
-    navbar.classList.toggle("active");
-    document.addEventListener("click", function(e) {
-        if(
-            !e.composedPath().includes(menuBtn) &&
-            !e.composedPath().includes(navbar)
-        ) {
-            navbar.classList.remove("active");
-        }
-    })
+// Navbar Start
+document.getElementById("menu-btn").addEventListener("click", function(){
+    var navbar = document.getElementById("navbar");
+    if (navbar.style.right === "8rem") {
+        navbar.style.right = "-250px";
+    } else {
+        navbar.style.right = "8rem";
+    }
 })
